bd7f869fb82e9f794e02e8c1028443a91f08d6da
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2021-01-02 20:07:28 +0700

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,7 +1,7 @@
 {
 	"contact": "phandin1951@gmail.com",
 	"id": "Touhou-Duck-Sound-Shot",
-	"patches": {"Duck Sound Damage Shot": (v1.0) Working Touhou 13 Ten Desires! Makes you have a good laugh!},
+	"patches": {"Duck Sound Damage Shot": (v1.0) (Working Touhou 13 Ten Desires! Makes you have a good laugh!)},
 	"servers": [
 		"https://raw.githubusercontent.com/GensokyoMegaBLACKLABEL/Touhou-Duck-Sound-Shot/main"
 	],
