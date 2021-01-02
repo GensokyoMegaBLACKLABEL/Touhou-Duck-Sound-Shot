412800afaf70dbd36fc918999e29af481b60353f
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2021-01-02 16:54:01 +0700

    Create patch.js

---
+++ Duck Sound Shot/patch.js
@@ -0,0 +1,12 @@
+{
+    "id": "Touhou-Duck-Sound-Shot",
+    "title": "Duck Quack Touhou Shot",
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "nmlgc/script_latin",
+        "nmlgc/western_name_order"
+    ],
+    "update": true,
+    "servers": [
+        "https://raw.githubusercontent.com/GensokyoMegaBLACKLABEL/Touhou-Duck-Sound-Shot/main",
+    ]
