b07bd1e572707f6ad390641f33c390f4a9ce17c9
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2021-01-02 20:19:55 +0700

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,8 +1,9 @@
 {
 	"contact": "phandin1951@gmail.com",
-	"id": "Touhou-Duck-Sound-Shot",
-	"patches": {"Duck Sound Damage Shot": "(v1.0) (Working Touhou 13 Ten Desires! Makes you have a good laugh!")},
+	"id": "Auto Thailand",
+	"patches": {"Duck Sound Damage Shot": "(v1.0) Working Touhou 13 Ten Desires! Makes you have a good laugh!"},
 	"servers": [
+		"https://mirrors.thpatch.net/
 		"https://raw.githubusercontent.com/GensokyoMegaBLACKLABEL/Touhou-Duck-Sound-Shot/main"
 	],
 	"title": "Auto's Patch Repo"
