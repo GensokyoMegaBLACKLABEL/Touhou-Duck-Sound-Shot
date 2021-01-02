e6fb08ca894e7a835cc753660202e4e82522ebee
GensokyoMegaBLACKLABEL <75157503+GensokyoMegaBLACKLABEL@users.noreply.github.com>
2021-01-02 19:57:46 +0700

    Add files via upload

---
+++ repo.js
@@ -0,0 +1,9 @@
+{
+	"contact": "phandin1951@gmail.com",
+	"id": "Touhou-Duck-Sound-Shot",
+	"patches": {},
+	"servers": [
+		"https://raw.githubusercontent.com/GensokyoMegaBLACKLABEL/Touhou-Duck-Sound-Shot/main"
+	],
+	"title": "Auto's Patch Repo"
+}
